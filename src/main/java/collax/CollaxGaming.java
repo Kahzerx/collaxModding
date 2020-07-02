362c8d36632d4bb4e787a3df09fea77245447bad
Kahzerx <kahzer123@gmail.com>
2020-07-02 22:23:19 +0200

    randomCoordsCommand added

--- src/main/java/collax/CollaxGaming.java
+++ src/main/java/collax/CollaxGaming.java
@@ -1,6 +1,7 @@
 package collax;
 
 import collax.discord.DiscordCommand;
+import collax.discord.RandomTpCommand;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.ServerCommandSource;
 
@@ -8,5 +9,6 @@ public class CollaxGaming {
     public static String[] bannedWords = {"c", "s", "!!skull", "!!tp", "!!comandos", "!!comandos2", "!!comandos3", "!!resethome"};
     public static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher){
         DiscordCommand.register(dispatcher);
+        RandomTpCommand.register(dispatcher);
     }
 }
---
+++ src/main/java/collax/discord/RandomTpCommand.java
@@ -0,0 +1,48 @@
+package collax.discord;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.command.arguments.EntityArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.GameMode;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.World;
+
+import java.util.Optional;
+import java.util.Random;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class RandomTpCommand {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher){
+        dispatcher.register(literal("randomCoords").
+                requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2)).
+                then(CommandManager.argument("playerName", EntityArgumentType.entity()).
+                        executes(context -> f0(context.getSource(), EntityArgumentType.getEntity(context, "playerName")))));
+    }
+
+    public static int f0(ServerCommandSource source, Entity playerName) throws CommandSyntaxException {
+        if (playerName instanceof ServerPlayerEntity){
+            Random rand = new Random();
+            double X = -10000 + (10000 + 10000) * rand.nextDouble();
+            double Z = -10000 + (10000 + 10000) * rand.nextDouble();
+            double Y = 255;
+            playerName.teleport(X, Y, Z);
+            BlockPos pos1 = source.getWorld().getTopPosition(Heightmap.Type.WORLD_SURFACE, playerName.getBlockPos());
+            playerName.teleport(X, pos1.getY(), Z);
+            ((ServerPlayerEntity) playerName).setSpawnPoint(World.OVERWORLD, playerName.getBlockPos(), true, false);
+            ((ServerPlayerEntity) playerName).sendMessage(new LiteralText("Recuerda usar el setHome, más información en !!comandos2"), false);
+        }
+        return 1;
+    }
+}

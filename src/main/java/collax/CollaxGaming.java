47e83aab6675fddbc48d0544aad433f9eb82d346
Kahzerx <kahzer123@gmail.com>
2020-07-03 14:05:40 +0200

    everyone can use /seed

--- src/main/java/collax/CollaxGaming.java
+++ src/main/java/collax/CollaxGaming.java
@@ -7,7 +7,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.ServerCommandSource;
 
 public class CollaxGaming {
-    public static String[] bannedWords = {"c", "s", "!!skull", "!!tp", "!!comandos", "!!comandos2", "!!comandos3", "!!resethome"};
+    public static String[] bannedWords = {"c", "C", "s", "S", "!!skull", "!!tp", "!!comandos", "!!comandos2", "!!comandos3", "!!resethome"};
     public static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher){
         DiscordCommand.register(dispatcher);
         RandomTpCommand.register(dispatcher);
